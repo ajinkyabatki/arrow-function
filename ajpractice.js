a1de7792c0cc2107e53f6b2d4e4475fa026987e8
ajinkyabatki <108880776+ajinkyabatki@users.noreply.github.com>
2022-08-05 07:27:54 +0530

    Add files via upload

---
+++ ajpractice.js
@@ -0,0 +1,35 @@
+
+
+// practice
+
+let arr=[1,2,3,4,5,6,7,8,9,10,11,12]
+/*let odd=arr.filter(number=> number%2 ) 
+
+console.log(odd);*/
+/*
+var result=
+"this is very interesting".replace(/\b[a-z]/g, (x)=> x.toUpperCase())
+console.log(result);
+*/
+/*
+const sum=arr.reduce((total,number)=>total+number,0);
+console.log(sum);
+*/
+/*
+arr=arr.filter((number)=>{
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
+}
+return true;
+});
+console.log(arr);
+*/
+/*
+array1=['foo', 'racecar', 'pineapple', 'porcupine', 'pineenip']
+function checkPalindrome(str){
+    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
+}
+const result = array1.filter(word => checkPalindrome(word));
+console.log(result);
+*/
+// displayarray.apply(this, arr);
